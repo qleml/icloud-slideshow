b2a52a572c358467a09072a5b24022869f8b2387
qleml <cchristoph@student.ethz.ch>
2024-12-25 20:01:20 +0000

    Remove build and log

--- scripts/fetch_media.sh
+++ scripts/fetch_media.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-ICLOUD_SLIDESHOW_PATH=/home/ccc/dev/photo-stream
+
+# Check if ICLOUD_SLIDESHOW_PATH is set
+if [ -z "$ICLOUD_SLIDESHOW_PATH" ]; then
+    echo "Error: ICLOUD_SLIDESHOW_PATH is not set."
+    exit 1
+fi
+
 FETCH_SCRIPT_DIR=$ICLOUD_SLIDESHOW_PATH/src
 PYTHON_FILE=fetch_media.py
-DIST_DIR=$ICLOUD_SLIDESHOW_PATH/dist/
-EXECUTABLE=fetch_media_executable
-LOG_DIR=$ICLOUD_SLIDESHOW_PATH/log/fetch_media.log
 
 export TERM=xterm
-HOME=/home/ccc
 source $ICLOUD_SLIDESHOW_PATH/venv/bin/activate
 
 # Fetch media
@@ -16,12 +18,6 @@ if python $FETCH_SCRIPT_DIR/$PYTHON_FILE $ICLOUD_SLIDESHOW_PATH; then
     echo "Media fetched successfully."
 else
     echo "Error fetching media. Continuing to restart feh."
-    if /bin/bash /home/ccc/dev/photo-stream/scripts/push_log.sh $LOG_DIR
-    then
-        echo "Log file pushed successfully."
-    else
-        echo "Error pushing log file."
-    fi
 fi
 
 # Check if a feh process is running
--- scripts/run_feh.sh
+++ scripts/run_feh.sh
@@ -3,17 +3,10 @@
 xhost +
 export DISPLAY=:0.0
 
-# Function to read the slideshow interval from config.yaml
-get_slideshow_interval() {
-    local interval=$(grep 'SLIDESHOW_INTERVAL' $ICLOUD_SLIDESHOW_PATH/config.yaml | awk '{print $2}')
-    echo $interval
-}
-
 # Function to run feh
 run_feh() {
-    local interval=$(get_slideshow_interval)
-    echo "Starting feh with slideshow interval of $interval seconds..."
-    feh --slideshow-delay $interval --recursive -F $ICLOUD_SLIDESHOW_PATH/media-rotated/
+    echo "Starting feh..."
+    feh --slideshow-delay 7 --recursive -F $ICLOUD_SLIDESHOW_DIR/media-rotated/
 }
 
 # Start the feh process
